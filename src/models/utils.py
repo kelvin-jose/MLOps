ea380dea501bdbd8aa961ceee9af5cbc8b486f09
Kelvin <kelvinkonoor@gmail.com>
2025-04-14 23:12:47 +0530

    metrics calculation

---
+++ src/models/utils.py
@@ -0,0 +1,8 @@
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    preds = logits.argmax(axis = -1)
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average = 'weighted')
+    acc = accuracy_score(labels, preds)
+    return {"accuracy": acc, "f1": f1, "precision": precision, "recall": recall}
